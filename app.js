166402877670e5f9f9ae4e70bc6142c379ada993
Nicolai <nicolai@mmg.io>
2017-01-27 14:54:00 +0000

    Learning about inner functions and outer functions for the sake of closures

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ var birds = 3;
 
 function dogHouse() {
   var dogs = 8;
-  console.log(birds); // 3
-  console.log(dogs); // 8
+  function showDogs(){
+   console.log(dogs);
+  }
+  return showDogs;
 }
 
-console.log(birds); // 3
-console.log(dogs); // undefined
+var getDogs = dogHouse();
+
+getDogs(); // 8
