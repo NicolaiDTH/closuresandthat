8482dd66d4b9548d316704334742b74562a3447d
Nicolai <nicolai@mmg.io>
2017-01-27 15:19:57 +0000

    refactoring

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-var birds = 3;
+// var birds = 3;
 
-function dogHouse() {
-  var dogs = 8;
-  function showDogs(){
-   console.log(dogs);
-  }
-  return showDogs;
+// function dogHouse() {
+//   var dogs = 8;
+//   function showDogs(){
+//    console.log(dogs);
+//   }
+//   return showDogs;
+// }
+
+// var getDogs = dogHouse();
+
+// getDogs(); // 8
+
+function makeCounter(noun) {
+ var count = 0;
+ return function() {
+  count += 1;
+  return count + '  ' + noun;
+ }
 }
-
-var getDogs = dogHouse();
-
-getDogs(); // 8
